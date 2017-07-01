ea9dde21911c3a02cb04e261976191e2d890ccc9
Tobias Obermayer <tobias.obermayer@gmail.com>
2017-07-01 18:43:09 +0200

    solved lession 3

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -7,24 +7,32 @@
     Sara has label 0
     Chris has label 1
 """
-    
+import os
 import sys
 from time import time
-sys.path.append("../tools/")
-from email_preprocess import preprocess
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+sys.path.append("../tools/")
+
+from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
 
+clf = SVC(kernel='rbf', C=10000.0)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+acc = accuracy_score(pred, labels_test)
+print(acc)
+print(list(pred).count(1), " == ", pred.sum())
+print(pred[10], pred[26], pred[50])
+print(pred[10]," ",  pred[26]," ", pred[50])    
