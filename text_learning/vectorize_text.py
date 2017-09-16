d002ab483b3ab48d5b2dfa2da0c03adc52ea52e9
Tobias Obermayer <tobias.obermayer@gmail.com>
2017-09-16 12:43:42 +0200

    text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
-
 import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
+
+# import os
 import pickle
 import re
 import sys
@@ -41,25 +44,29 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
+            path = os.path.join('R:\\', path[:-1]).replace('.', '_')
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            text = parseOutText(email)
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            signature_words = ["sara", "shackleton", "chris", "germani"]
+
+            for word in signature_words:
+                text = text.replace(word, "")
 
             ### append the text to word_data
-
+            word_data.append(text)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(0 if name == "sara" else 1)
 
             email.close()
 
 print "emails processed"
+print("word_data[152]: \n{0}".format(word_data[152]))
 from_sara.close()
 from_chris.close()
 
@@ -72,4 +79,11 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 ### in Part 4, do TfIdf vectorization here
 
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+vectorizer = TfidfVectorizer("english", stop_words="english")
+
+vectorizer.fit(word_data)
+vectorizer.transform(word_data)
+print("length .get_feauture_names(): {0}".format(len(vectorizer.get_feature_names())))
+print(vectorizer.get_feature_names()[34597])
--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,14 +28,16 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
+        from nltk.stem.snowball import SnowballStemmer
 
+        stemmer = SnowballStemmer("english")
 
+        words = " ".join([stemmer.stem(x) for x in text_string.split()])
 
 
     return words
@@ -50,5 +52,9 @@ def main():
 
 
 if __name__ == '__main__':
+    import os
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dir_path)
+
     main()
 
