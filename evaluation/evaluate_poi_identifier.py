d6bf4c0a0be7300b54f7b5e19f8cbfaa360b45e9
Tobias Obermayer <tobias.obermayer@gmail.com>
2017-09-27 20:45:49 +0200

    elvaluate chapter

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -10,6 +10,9 @@
 
     Start by loading/formatting the data...
 """
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(dir_path)
 
 import pickle
 import sys
@@ -29,3 +32,37 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print(clf.score(features_test, labels_test))
+
+prediction = clf.predict(features_test)
+print(sum(prediction))
+print(len(features_test))
+print((len(features_test)-sum(prediction))/(1.*len(features_test)))
+print(sum(prediction*labels_test))
+
+from sklearn.metrics import precision_score, recall_score
+
+print("precision_score", precision_score(labels_test, prediction))
+print("recall_score", recall_score(labels_test, prediction))
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+import numpy as np
+true_positiv = sum(np.multiply(true_labels, predictions))
+print("true_positiv", true_positiv)
+true_negativ = sum([1 for x, y in zip(true_labels, predictions) if x == 0 and y == 0])
+print("true_negativ", true_negativ)
+false_positiv = sum([1 for x, y in zip(true_labels, predictions) if x == 0 and y == 1])
+print("false_positiv", false_positiv)
+false_negativ = sum([1 for x, y in zip(true_labels, predictions) if x == 1 and y == 0])
+print("false_negativ", false_negativ)
+precision = (1. * true_positiv) / (true_positiv + false_positiv)
+print("precision", precision)
+recall = (1. * true_positiv) / (true_positiv + false_negativ)
+print("recall", recall)
